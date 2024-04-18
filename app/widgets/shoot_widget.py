162032cbac847a6acfc24d7648ae3171f1884d4c
nach155 <nagahaya.0603@gmail.com>
2024-04-18 23:12:17 +0900

    レイアウト修正

--- app/widgets/shoot_widget.py
+++ app/widgets/shoot_widget.py
@@ -1,5 +1,5 @@
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QWidget, QPushButton, QVBoxLayout
+from PyQt6.QtWidgets import QWidget, QPushButton, QVBoxLayout, QHBoxLayout
 
 class ShootWidget(QWidget):
     def __init__(self) -> None:
@@ -9,13 +9,20 @@ class ShootWidget(QWidget):
     def initialize_UII(self) -> None:
         
         self.camera_start_button = QPushButton("カメラ起動")
+        self.camera_stop_button = QPushButton("カメラ停止")
+        camera_layout = QHBoxLayout()
+        camera_layout.addWidget(self.camera_start_button)
+        camera_layout.addWidget(self.camera_stop_button)
         
         self.shoot_start_button = QPushButton("検知開始")
         self.shoot_stop_button = QPushButton("検知終了")
         
+        shoot_layout = QHBoxLayout()
+        shoot_layout.addWidget(self.shoot_start_button)
+        shoot_layout.addWidget(self.shoot_stop_button)
+        
         self.layout = QVBoxLayout()
-        self.layout.addWidget(self.camera_start_button)
-        self.layout.addWidget(self.shoot_start_button)
-        self.layout.addWidget(self.shoot_stop_button)
+        self.layout.addLayout(camera_layout)
+        self.layout.addLayout(shoot_layout)
         
         self.setLayout(self.layout)
